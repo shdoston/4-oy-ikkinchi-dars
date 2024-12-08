f06ab22c4f2c0c65354520a4873da0f95da9c033
Dostonbek <dostonbek200gmail.com>
2024-12-08 20:15:34 +0500

    if else

--- app.js
+++ app.js
@@ -5,11 +5,58 @@
 //     alert (myName + ',sayitimizga hush kelibsiz' )
 // }
 
-let number = prompt("raqam kiriting")
-if (number%3){
-    alert('fizz')
-} else if(number%5){
-    alert('bazz')
-} else if(number){
-    alert("fizzbizz")
+// let number = prompt("raqam kiriting")
+// if (number%3){
+//     alert('fizz')
+// } else if(number%5){
+//     alert('bazz')
+// } else if(number){
+//     alert("fizzbizz")
+// }
+// let a = 5
+// let b = 2
+// if (a>2 && b<=3){
+//     console.log('jumla rost')
+// }else{
+//     console.log('yolgon')
+// }
+
+// let a = 3
+// let b = 4
+// let c = 5
+
+// if ( a <=b && b <= c){
+//     console.log("jumla rost")
+// } else {
+//     console.log("jumla yolg'on")
+// }
+
+// let a = 8
+// let b = 6
+// if (a % 2 == 0 && b % 2 === 0){
+//     console.log("ikkalasi ham jujt jumla rost rost")
+// } else {
+//     console.log('ikkalasi ham toq')
+// }
+
+// let a = -1
+// let b = 3
+// let c = 4
+// if ( a > 0 || b > 0 || c > 0){
+//     console.log("hech bo'lmaganda bitasi manfiy jumla rost")
+// } else{
+//     console.log("hammasi manfiy")
+// }
+
+// let a = 6
+// if ( a > 0){
+//     console.log('musbat son')
+// } else {
+//     console.log('manfiy son')
+// }
+let b = 3
+if ( b % 2 !== 0) {
+    console.log("toq son")
+} else {
+    console.log('juft son')
 }
